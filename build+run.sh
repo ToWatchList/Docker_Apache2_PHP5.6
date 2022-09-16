39ec1e62a13f27cfef3ef53a111ebbc7462440e6
Nick Vance <nick@towatchlist.com>
2022-09-16 11:37:09 -0700

    add link, comment out local build & run by default

--- build+run.sh
+++ build+run.sh
@@ -6,21 +6,22 @@ docker rm apache2-container
 set -xe
 
 # pull the latest base image
+# https://hub.docker.com/r/ubuntu/apache2
 docker pull ubuntu/apache2:2.4-22.04_beta
 
-## BUILD AND RUN LOCAL
-docker build . -t towatchlist/apache2php5.6
+# ## BUILD AND RUN LOCAL
+# docker build . -t towatchlist/apache2php5.6
 
-docker run -d \
-    --name apache2-container \
-    -e TZ=UTC -p 8080:80 \
-    -v "$(pwd)/site:/var/www/html" \
-    -v "$(pwd)/apache2.conf:/etc/apache2/apache2.conf" \
-    towatchlist/apache2php5.6
+# docker run -d \
+#     --name apache2-container \
+#     -e TZ=UTC -p 8080:80 \
+#     -v "$(pwd)/site:/var/www/html" \
+#     -v "$(pwd)/apache2.conf:/etc/apache2/apache2.conf" \
+#     towatchlist/apache2php5.6
 
-sleep 1
-open http://localhost:8080/phpversion.php
-exit
+# sleep 1
+# open http://localhost:8080/phpversion.php
+# exit
 
 ## MULTI ARCH BUILD AND PUSH TO DOCKERHUB
 docker buildx build \
